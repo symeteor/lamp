fa6bf4b89372d3db9059f6432939bca882c5c674
symeteor <44577442+symeteor@users.noreply.github.com>
2019-04-03 17:28:21 +0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Hello, Docker! From Git.hub 3. </p>
+  <?php echo '<p>Hello World</p>'; ?>
+  <p>Hello, Docker! From Git.hub 4. </p>
+  <?php
+$mysql = new mysqli("localhost","root");
+echo "MySQL Server info: ".$mysql->host_info;
+?>
+</body>
+</html>
