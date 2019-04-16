c1c7d2dad8515ce28d0a22df1ddd37775f28c10f
symeteor <44577442+symeteor@users.noreply.github.com>
2019-04-16 18:26:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <body>
-<p>Hello, Docker! From Git.hub 3. </p>
+<p>Hello, Docker! From Git.hub Restart. </p>
   <?php echo '<p>Hello World</p>'; ?>
   <p>Hello, Docker! From Git.hub 4. </p>
   <?php
 $mysql = new mysqli("localhost","root");
 echo "MySQL Server info: ".$mysql->host_info;
+echo "MySQL DataBases: ".$mysql->host_info;
 ?>
 </body>
 </html>
